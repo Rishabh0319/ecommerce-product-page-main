8c02ce5d766067eb5d6a9734dad2b6cb2f12d32c
Rishabh Tripathi <rishabhtripathi0319@gmail.com>
2022-07-25 13:20:43 +0530

    version 5: project allmost compleat 95% and mobile responsive design is also work properly

--- js/script.js
+++ js/script.js
@@ -15,6 +15,12 @@ const titleData = document.querySelector('.titleData');
 const priceData = document.querySelector('.priceData');
 const quantityData = document.querySelector('.quantityData');
 const totalPriceData = document.querySelector('.totalPriceData');
+const fullView = document.querySelector('.full-view');
+const fullViewImage = document.querySelector('.full-view #productImage');
+const closeFullView = document.querySelector('.closeFullView');
+
+
+
 
 /* ******************* CART DATABASE ********************  */
 let cartItemData = {
@@ -22,24 +28,25 @@ let cartItemData = {
     price: 0,
     quentity: 0,
     totalPrice: 0
-  }
+}
 
 /* *******************  ********************  */
 
 /* ******************* change the image of productImage by clicking on another option ********************  */
 
-imageArray.forEach((image)=>{
-   image.addEventListener('click', ()=>{
-    // Get the images list src address on click event
-    const selectImage = image.childNodes[0].attributes.src.value;   
-    productImage.src = selectImage;
-   })
+imageArray.forEach((image) => {
+    image.addEventListener('click', () => {
+        // Get the images list src address on click event
+        const selectImage = image.childNodes[0].attributes.src.value;
+        productImage.src = selectImage;
+        fullViewImage.src = selectImage;
+    })
 })
 
 
 
 
-cartButton.addEventListener('click', ()=>{
+cartButton.addEventListener('click', () => {
     displayCart();
 })
 
@@ -48,17 +55,15 @@ cartButton.addEventListener('click', ()=>{
 /* ******************* this function is display the cart frame ********************  */
 
 let cartOff = true;
-function displayCart() 
-{
-    if(cartOff === true)      // cart show/hide logical condition
+function displayCart() {
+    if (cartOff === true)      // cart show/hide logical condition
     {
-        cartframe.style.display = 'block'; 
-        cartframe.style.backgroundColor = '#ffffff'; 
+        cartframe.style.display = 'block';
+        cartframe.style.backgroundColor = '#ffffff';
         cartOff = false;
     }
-    else
-    {
-        cartframe.style.display = 'none'; 
+    else {
+        cartframe.style.display = 'none';
         cartOff = true;
     }
     checkCart();    // call function to check cart is empty or not
@@ -68,23 +73,19 @@ function displayCart()
 /* ******************* this function is check cart is empty or not ********************  */
 
 
-function checkCart() 
-{
-    if(cartItemData.quentity === 0)
-    {
+function checkCart() {
+    if (cartItemData.quentity === 0) {
         cartMessage.style.display = 'block'
     }
-    else
-    {
-        cartMessage.style.display = 'none'; 
+    else {
+        cartMessage.style.display = 'none';
         displayItemOnCart();
         cartItemContainer.style.display = 'flex';
     }
 }
 
 
-function displayItemOnCart() 
-{
+function displayItemOnCart() {
     titleData.innerHTML = cartItemData.title;
     priceData.innerHTML = cartItemData.price;
     quantityData.innerHTML = cartItemData.quentity;
@@ -94,35 +95,33 @@ function displayItemOnCart()
 
 /* ******************* this function is indicates the product quantity between plus/minus button  ********************  */
 
-function IncDecQuentityIndicator()
-{
-   PMquantity.innerHTML = cartItemData.quentity;
+function IncDecQuentityIndicator() {
+    PMquantity.innerHTML = cartItemData.quentity;
 }
 
 /* ******************* this function is calculate the quantity of item and price ********************  */
 
-function calculate()    
-{
-    
-   if(cartItemData.quentity > 0 && cartItemData.quentity < 2) 
-   {
-     cartItemData.price = 125.00;    
+function calculate() {
 
-     cartItemData.totalPrice = (cartItemData.price * cartItemData.quentity);
-   }
+    if (cartItemData.quentity > 0 && cartItemData.quentity < 2) {
+        cartItemData.price = 125.00;
 
-   console.log("title: " + cartItemData.tital + " " + "price: " + cartItemData.price + " " + "quantity: " + cartItemData.quentity + " " + "totalPrice: " + cartItemData.totalPrice);    // it indicates the value of database
+        cartItemData.totalPrice = (cartItemData.price * cartItemData.quentity);
+    }
 
-   indicator();  // call to show the quantity of product in cart
-   IncDecQuentityIndicator();  // call to show quantity of product between plus/minus button
+    console.log("title: " + cartItemData.tital + " " + "price: " + cartItemData.price + " " + "quantity: " + cartItemData.quentity + " " + "totalPrice: " + cartItemData.totalPrice);    // it indicates the value of database
+
+    indicator();  // call to show the quantity of product in cart
+    IncDecQuentityIndicator();  // call to show quantity of product between plus/minus button
+    displayItemOnCart();  // to update the cart data
 }
 
 
 /* ******************* this event is call when user click on addtocart button ********************  */
 
-addToCartButton.addEventListener('click',()=>{
+addToCartButton.addEventListener('click', () => {
 
-    if(cartItemData.quentity == 0) // this condition is increment indicator only one time when user click on add to cart
+    if (cartItemData.quentity == 0) // this condition is increment indicator only one time when user click on add to cart
     {
         cartItemData.quentity = cartItemData.quentity + 1;  // increment the quentity of product
         cartItemData.title = 'Fall Limited Edition Sneakers';
@@ -133,25 +132,22 @@ addToCartButton.addEventListener('click',()=>{
         addToCartButton.style.cursor = 'not-allowed';
         checkCart();
     }
-    else
-    {
+    else {
         addToCartButton.style.backgroundColor = 'hsl(26, 100%, 55%)';
         addToCartButton.style.cursor = 'pointer';
     }
-    
+
 })
 
 
 /* ******************* this function is indicates the cart quantity in orange color ********************  */
 
 function indicator() {
-    if(cartItemData.quentity != 0 && cartItemData.quentity > 0)
-    {
+    if (cartItemData.quentity != 0 && cartItemData.quentity > 0) {
         cartQuantityIndecator.innerHTML = cartItemData.quentity;      //this logic increase indicator value by one
         cartQuantityIndecator.style.display = 'flex';
     }
-    else
-    {
+    else {
         cartQuantityIndecator.style.display = 'none';
     }
 }
@@ -159,16 +155,13 @@ function indicator() {
 
 /* ******************* this event occures when user want's to increase the value of  ********************  */
 
-plus.addEventListener('click', ()=>{
+plus.addEventListener('click', () => {
 
-    console.log('you clicked on plus button');
-
-    if(cartItemData.quentity >= 1)    // this logic is define for increment only when product quantity is greaterOrEqualone (>= 1)
+    if (cartItemData.quentity >= 1)    // this logic is define for increment only when product quantity is greaterOrEqualone (>= 1)
     {
         cartItemData.quentity = cartItemData.quentity + 1;
         cartItemData.totalPrice = (cartItemData.price * cartItemData.quentity);
         calculate();
-        displayItemOnCart();
     }
 });
 
@@ -176,19 +169,32 @@ plus.addEventListener('click', ()=>{
 
 /* ******************* this event occures when user want's to increase the value of  ********************  */
 
-minus.addEventListener('click', ()=>{
-    if(cartItemData.quentity > 1)
-    {
+minus.addEventListener('click', () => {
+    if (cartItemData.quentity > 1) {
         cartItemData.quentity = cartItemData.quentity - 1;
         cartItemData.totalPrice = (cartItemData.price * cartItemData.quentity);
         calculate();
-        displayItemOnCart();
     }
 })
 
 
 
+/* ******************* this event occures when you click on product Image  ********************  */
 
+productImage.addEventListener('click',()=>{
+
+    fullView.style.display = 'flex';
+
+});
+
+
+
+/* ******************* this event occures when you click on closed button of fullview  ********************  */
+
+closeFullView.addEventListener('click', ()=>{
+    console.log('closed');
+    fullView.style.display = 'none';
+})
 
 
 
